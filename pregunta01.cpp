a2a3e665dede2668d53a801b9855ba621ff2aca9
EVER1 <ever_SR_1@hotmail.com>
2026-01-25 23:44:14 -0500

    Apartir de estos archivos desarrollare las 4 preguntas de la pc N°02

---
+++ pregunta01.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+// Complete la funcion que calcula la derivada de orden k de un polinomio
+// Usar EXCLUSIVAMENTE punteros (NO indices)
+
+void derivadaK(
+    const double* coef, //
+    int grado, int k,   //
+    double* res,        //
+    int& gradoRes       //
+) {
+    // Escriba su codigo aqui
+    gradoRes = grado - k;
+
+}
+
+
+
+
+// Complete la función auxiliar para imprimir un polinomio
+void imprimirPolinomio(const double* p, int grado) {
+   
+}
+
+int main() {
+    // Polinomio:
+    // P(x) = 2 + 3x - x^2 + 4x^3
+    double coef[] = {2, 3, -1, 4};
+    const int grado = 3;
+
+    double resultado[10]; // espacio suficiente
+    int gradoResultado;
+
+    int k = 2; // derivada de orden 2
+
+    // Llamar a su funcion derivadaK
+
+    // Llamar la funcion imprimir para mostrar el polinomio original y  la derivada
+    
+
+    return 0;
+}
---
+++ pregunta02.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+// Complete la funcion de busqueda
+int* buscar(
+    int* inicio,                 // Estos dos primeros parametros
+    int* fin,                    // delimitan el rango de busqueda a [inicio, fin)
+    bool (*equivale)(int, int),  // equivale es una funcion que determina cuando dos enteros se condideran equivalentes
+    int clave                    // clave es el valor a buscar segun el criterio indicado
+) {
+    // Complete aqui 
+    
+}
+
+// Implemente aqui los Criterios de equivalencia
+// y si es necesario algunas funciones adicionales para reproducir el ejemplo de salida
+
+
+
+int main() {
+    int A[] = { -1, 21, 2, -2, 8, 13, 25 };
+    int* inicio = A;
+    int* fin = A + 7;
+
+    int clave = 13;
+
+    // Imprima el arreglo
+
+
+    // Llame a la funcion buscar para cada criterio implementado
+
+ 
+
+    return 0;
+}
