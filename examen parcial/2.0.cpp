02920d41b8851fc1377fcf5ab3b8ce81d1976465
EVER1 <ever_SR_1@hotmail.com>
2026-02-09 22:57:58 -0500

    En la pregunta 2 se implementó funciones fuera del main para ingresar datos, ordenar y mostrar el Ranking

--- examen parcial/2.0.cpp
+++ examen parcial/2.0.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void ingresar_datos(int n,int *T,string *N){
+      for (int i = 0; i < n; i++){
+        cout << "INGRESE EL NOMBRE DEL CORREDOR " <<i+1<<": "<< endl; 
+          cin >> *(N+i);
+        cout << "INGRESE TIEMPO DEL CORREDOR " <<i+1<< endl;
+        cin >> *(T+i);
+    }};
+    
+    
+    void ordenar_Ranking(int n2,int *T,string *N){
+	for(int i=0; i< n2-1; i++){
+		for(int j=0;j<n2-1;j++){
+			if(*(T+j)>*(T+j+1)){
+				int temp= T[j];
+				T[j]=T[j+1];
+				T[j+1]=temp;//ordenando tiempo
+				
+				string aux=*(N+j);				
+				N[j]=N[j+i];
+				N[j+i+1]=aux;
+			}}}};
+			
+	void  mostrar_ranking (int n3, int *T,string *N){
+	cout << "\nRANKING FINAL" << endl;
+	 for (int i = 0; i < n3; i++) {
+        cout << i + 1 << ". " << *(N+i) << " - " << *(T+i) << " segundos" << endl;
+		}};
 
 
 int main(){
-   string N[30]; 
-    int T[30];
-      for (int i = 0; i < 5; i++){
-        cout << "INGRESE NOMBRE: "<< ; 
-        cin>>N[i];
-    }
+	int M=30;
+   string No[M]; 
+    int Ti[M];
+    int m;
+    int*T =T;
+    string *N =N;
+    
+    cout << "Ingrese numero de corredores: "; 
+	
+	cin >>m ;
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "INGRESE TIEMPO: " << ;
-        cin >> T[i];
-    }
-
-    for (int i = 0; i < 5; i++){
-    cout<< "Nombre del corredor "<< i+1<<": "<<N[i]; 
-    cout<< "tiempo del corredor "<< i+1<<": "<<T[i];}
-
-
+	ingresar_datos( m, Ti,No);
+ 	ordenar_Ranking(m,Ti,No);
+ 	mostrar_ranking (m,Ti,No);
+ 
 
     return 0;
 }
