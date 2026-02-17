4152c8d01fc52b19ff3af5d2389c6c7add0c0cfb
EVER1 <ever_SR_1@hotmail.com>
2026-02-16 23:10:33 -0500

    se realizo mejoras y se completo lo que se pedia en el ejercicio 1

--- pc4/1.cpp
+++ pc4/1.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 using namespace std;
 
+
+//estructura polinomio 
 struct polinomio {
 	int grado;
 	int *coeficientes;
@@ -10,18 +12,19 @@ struct polinomio {
 
 int main(){
 	
-	polinomio polinomio1;
-	polinomio polinomio2;
-	polinomio polinomio3;
+	//declaracion
+	polinomio polinomio1;//polinomio 1
+	polinomio polinomio2;//polinomio 2
+	polinomio polinomio3;//polinomio 3
 	polinomio polinomio4;//suma de tres polinomios
 	polinomio polinomio5;//multiplicacion de tres polinomios
 	
+	//grados inicializados de los polinomios
 	polinomio1.grado=2;
 	polinomio2.grado=2;
 	polinomio3.grado=2;
-	polinomio4.grado=2;
-	polinomio5.grado=6;  //(polinomio1.grado*polinomio2.grado*polinomio3.grado);
-	
+	polinomio4.grado=2;  //suma de polinomios
+	polinomio5.grado=(polinomio1.grado+polinomio2.grado+polinomio3.grado);
 	
 	//arreglo dinamico de coeficientes en orden decreciente
 	polinomio1.coeficientes=new int[polinomio1.grado+1];
@@ -30,11 +33,10 @@ int main(){
 	polinomio4.coeficientes=new int[polinomio4.grado+1];
 	polinomio5.coeficientes=new int[polinomio5.grado+1];
 	
-	
 	//inicicialización polinomi1:
 	polinomio1.coeficientes[0]=2;
 	polinomio1.coeficientes[1]=3;
-	polinomio1.coeficientes[2]=4;
+	polinomio1.coeficientes[2]=2;
 	
 	//inicicialización polinomio2:
 	polinomio2.coeficientes[0]=5;
@@ -44,7 +46,7 @@ int main(){
 	//inicicializacion polinomio3:
 	polinomio3.coeficientes[0]=6;
 	polinomio3.coeficientes[1]=3;
-	polinomio3.coeficientes[2]=4;
+	polinomio3.coeficientes[2]=8;
 	
 	//inicializacion polinomio 4:  suma de tres polinomios: 
 	polinomio4.coeficientes[0]=0;
@@ -81,83 +83,101 @@ int main(){
 		polinomio1.coeficientes[i]*polinomio2.coeficientes[j]*polinomio3.coeficientes[k];}	
 			}}
 			
-	
+			//impresion de polinomios
 			
 	//impresión de polinomio 1:
-cout<<polinomio1.coeficientes[0]<< "x^2  +";
-cout<<polinomio1.coeficientes[1]<< "x  +";
-cout<<polinomio1.coeficientes[2]<< "  ";
-cout<<endl;
+cout<<"polinomio 1 :"<<endl;
+	for(int i=0;i<polinomio1.grado+1;i++){
+			cout<<polinomio1.coeficientes[i];
+			if(i<polinomio1.grado){cout<<"x^"<<polinomio1.grado-i;}
+			if(i<polinomio1.grado){cout<<" + ";}}	
+			cout<<endl;
 
 	//impresión de polinomio 2:
-cout<<polinomio2.coeficientes[0]<< "x^2  +";
-cout<<polinomio2.coeficientes[1]<< "x  +";
-cout<<polinomio2.coeficientes[2]<< "  ";
-cout<<endl;
+cout<<"polinomio 2 :"<<endl;
+	for(int i=0;i<polinomio2.grado+1;i++){
+			cout<<polinomio2.coeficientes[i];
+			if(i<polinomio2.grado){cout<<"x^"<<polinomio2.grado-i;}
+			if(i<polinomio2.grado){cout<<" + ";}}	
+			cout<<endl;
+
 
 	//impresión de polinomio 3:
-cout<<polinomio3.coeficientes[0]<< "x^2  +";
-cout<<polinomio3.coeficientes[1]<< "x  +";
-cout<<polinomio3.coeficientes[2]<< "  ";
-cout<<endl;
+cout<<"polinomio 3 :"<<endl;
+	for(int i=0;i<polinomio3.grado+1;i++){
+			cout<<polinomio3.coeficientes[i];
+			if(i<polinomio3.grado){cout<<"x^"<<polinomio3.grado-i;}
+			if(i<polinomio3.grado){cout<<" + ";}}	
+			cout<<endl;
 
 	//impresion del polinomio 4: suma de tres polinomios.
-cout<<polinomio4.coeficientes[0]<< "x^2  +";
-cout<<polinomio4.coeficientes[1]<< "x  +";
-cout<<polinomio4.coeficientes[2]<< "  ";
-cout<<endl;	
+cout<<"polinomio 4 (suma de los tres polinomios) :"<<endl;
+		for(int i=0;i<polinomio4.grado+1;i++){
+			cout<<polinomio4.coeficientes[i];
+			if(i<polinomio4.grado){
+			cout<<"x^"<<polinomio4.grado-i;}
+			if(i<polinomio4.grado){cout<<" + ";}}	
+			cout<<endl;
 
 	//impresion del polinomio 5: producto de tres polinomios.
-cout<<polinomio5.coeficientes[0]<< "x^2  +";
-cout<<polinomio5.coeficientes[1]<< "x  +";
-cout<<polinomio5.coeficientes[2]<< "  ";
-cout<<endl;	
+cout<<"polinomio 5 (producto de los tres polinomios) :"<<endl;
+		for(int i=0;i<polinomio5.grado+1;i++){
+			cout<<polinomio5.coeficientes[i]<<"x^"<<polinomio5.grado-i;
+			if(i<polinomio5.grado){cout<<" + ";}}	
+			cout<<endl;
+
+
+			//coefientes de los polinomios
+			
+	cout<<"coeficientes de los polinomios : "<<endl;
 	
 	cout<<"coeficientes del polinomio 1: "<<endl;
 		cout<<"(";
-		for(int i=0;i<grado+1;i++){
+		for(int i=0;i<polinomio1.grado+1;i++){
 			cout<<polinomio1.coeficientes[i];
-			if(i<grado){
+			if(i<polinomio1.grado){
 			cout<<" , ";}}
 			cout<<")"<<endl;
-			
-			
+					
 	cout<<"coeficientes del polinomio 2: "<<endl;
 		cout<<"(";
-		for(int i=0;i<grado+1;i++){
+		for(int i=0;i<polinomio2.grado+1;i++){
 			cout<<polinomio2.coeficientes[i];
-			if(i<grado){
+			if(i<polinomio2.grado){
 			cout<<" , ";}}
 			cout<<")"<<endl;
 			
 	cout<<"coeficientes del polinomio 3: "<<endl;
 		cout<<"(";
-		for(int i=0;i<grado+1;i++){
+		for(int i=0;i<polinomio3.grado+1;i++){
 			cout<<polinomio3.coeficientes[i];	
-			if(i<grado){
+			if(i<polinomio3.grado){
 			cout<<" , ";}}
 			cout<<")"<<endl;
 			
-		cout<<"coeficientes del polinomio 4: "<<endl;
+		cout<<"coeficientes del polinomio 4: (suma de tres polinomios) "<<endl;
 		cout<<"(";
-		for(int i=0;i<grado+1;i++){
+		for(int i=0;i<polinomio4.grado+1;i++){
 			cout<<polinomio4.coeficientes[i];	
-			if(i<grado){
+			if(i<polinomio4.grado){
 			cout<<" , ";}}
 			cout<<")"<<endl;
 			
-		cout<<"coeficientes del polinomio 5: "<<endl;
+		cout<<"coeficientes del polinomio 5 (multiplicacion de 3 polinomios): "<<endl;
 		cout<<"(";
 		for(int i=0;i<polinomio5.grado+1;i++){
 			cout<<polinomio5.coeficientes[i];	
 			if(i<polinomio5.grado){
 			cout<<" , ";}}
 			cout<<")"<<endl;
-			
-			
-			cout<<polinomio5.coeficientes[5];
 
+//liberación de memoria dinamica
 
+delete[] polinomio1.coeficientes;
+delete[] polinomio2.coeficientes;
+delete[] polinomio3.coeficientes;
+delete[] polinomio4.coeficientes;
+delete[] polinomio5.coeficientes;
 
     return 0;
 }
